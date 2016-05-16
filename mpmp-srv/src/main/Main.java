6b0a533665b6775715a9f461de4faa0b44995d78
Leander Dreier <leander3@outlook.de>
2016-05-16 15:40:08 +0200

    added skeleton server

---
+++ mpmp-srv/src/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Leander
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
