7ab4899edfd7db4eff63ac970b9aa1e033ba8a1c
Leander Dreier <leander3@outlook.de>
2016-06-02 11:09:05 +0200

    removed ability to send chat before subscribing

--- mpmp-srv/src/model/Chatbuf.java
+++ mpmp-srv/src/model/Chatbuf.java
@@ -5,10 +5,10 @@ import main.Client;
 public class Chatbuf extends Msgbuf{
 	// XXX who needs a chat *buffer*? spec change?
 	public static void send(Client sender, String chat) {
+		if (sender.getName() == null)
+			return;
+		
 		for (Client c : main.Main.getClients())
-			if (sender.getName() == null)
-				c.send("chat-update 1\n" + "(???) " + chat);
-			else
-				c.send("chat-update 1\n" + "(" + sender.getName() + ") " + chat);
+			c.send("chat-update 1\n" + "(" + sender.getName() + ") " + chat);
 	}
 }
