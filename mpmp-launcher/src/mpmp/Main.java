61c78dc426d06ac6e4d38f1cf1c40bb11648646a
dieechtenilente <klaushackner@live.de>
2016-06-06 17:11:24 +0200

    #8 Created Launcher, no function yet

---
+++ mpmp-launcher/src/mpmp/Main.java
@@ -0,0 +1,13 @@
+package mpmp;
+
+/**
+ * @author Oskar, Klaus
+ */
+public class Main {
+    public static void main(String[] args) {
+        View v;
+        v = new View();
+        v.setVisible(true);
+        System.out.println("Launcher showed.");
+    }
+}
