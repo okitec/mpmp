35dd6aaf532c6aa7405854c7fb36e733d7269ea7
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-06-17 23:55:54 +0200

    add skeletal model.Card

---
+++ mpmp/src/model/Card.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Card {
+	private final String text;
+
+	public Card(String text, String actions) {
+		this.text = text;
+		// XXX parse actions and save as p-code
+	}
+}
