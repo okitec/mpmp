022372809f88b867d97b03cd21e482c420daec6e
Leander Dreier <leander3@outlook.de>
2016-06-20 19:39:42 +0200

    Added batch script to make testing without the launcher easier

---
+++ startmpmp.bat
@@ -0,0 +1,9 @@
+del mpmp.jar
+copy mpmp\dist\mpmp.jar mpmp.jar
+del mpmp-launcher.jar
+copy mpmp-launcher\dist\mpmp-launcher.jar mpmp-launcher.jar
+cls
+start java -jar mpmp.jar server
+start ..\ncat.exe localhost 1918
+start java -jar mpmp.jar client localhost player #000000 test1
+start java -jar mpmp.jar client localhost player #FFFFFF test2
