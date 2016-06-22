b2b88de6f0749e6e319c4bfba253777645021ed8
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-06-22 16:32:19 +0200

    added very special fields

--- mpmp/src/model/Field.java
+++ mpmp/src/model/Field.java
@@ -6,7 +6,11 @@ package model;
  * @author oki
  */
 public class Field {
-	public static final int Prison = 10;
+	public static final int IncomeTax   =  4;
+	public static final int Prison      = 10;
+	public static final int FreeParking = 20;
+	public static final int Police      = 30;
+	public static final int ExtraTax    = 38;
 
-	public static final int Nfields = 40;
+	public static final int Nfields     = 40;
 }
