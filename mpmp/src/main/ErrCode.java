cf271b2c993676263cf91feb6f7e67dff9043fbc
Leander Dreier <leander3@outlook.de>
2016-06-23 20:39:58 +0200

    Added ErrCodes from proto

--- mpmp/src/main/ErrCode.java
+++ mpmp/src/main/ErrCode.java
@@ -9,12 +9,20 @@ package main;
  *		x0z  EOF
  *		x1z  Subscribing, ClientlistUpdating
  *		x2z  Chatting, Whispering
+ *		x3z  Gameplay
  *	third digit: What exactly happened?
  *		depends on the second digit
  * 
  * @author Leander
  */
 public enum ErrCode {
+	MissingMoney(131, "You don't have enough money for this"),
+	MissingUnjailCard(132, "You don't have a unjail card"),
+	NewEventCard(133, "I want to have a new eventcard"),
+	NewEventCardImossible(134, "You can't get a new eventcard"),
+	UnbalancedColor(135, "Your houses are to unbalanced for that color"), //XXX Say what color?
+	AlreadyOwned(136, "This is owned by another player"),
+	PlotWithHouse(137, "You can't sell a plot with houses on it"),
 	EOF(201, "Unexpected EOF"),
 	NameTaken(211, "Name already taken!"),
 	ClientlistUpdateUsage(212, "Usage: clientlist-update <number of clients>"),
@@ -22,7 +30,8 @@ public enum ErrCode {
 	SubscribeUsage(214, "Usage: subscribe <spectator|player> #<rgb hex triplet> <Name>"),
 	NotSubscribed(221, "You are not subscribed"),
 	NoSuchPlayer(222, "This player does not exist"),
-	WhisperUsage(223, "Usage: whisper <playername> <message>");
+	WhisperUsage(223, "Usage: whisper <playername> <message>"),
+	UnjailUsage(231, "Usage: unjail [card|money]");
 	
 	private final int code;
 	private final String message;
