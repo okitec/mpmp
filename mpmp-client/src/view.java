83e44ad6022c81710974549ab9c465b47456d046
dieechtenilente <klaushackner@live.de>
2016-06-26 00:01:45 +0200

    Make SVG great again! (Take 2)

--- mpmp-client/src/view.java
+++ mpmp-client/src/view.java
@@ -1,17 +1,16 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
-import javafx.scene.layout.Background;
 import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -35,30 +34,22 @@ public class view extends javax.swing.JFrame {
 
     public view() {
 	initComponents();
+	setLayout(new BorderLayout());
 	canvas = new JSVGCanvas();
+	canvas.setURI(new File("src/res/gameboard.svg").toURI().toString());
 
-	
 	try {
-     		setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/res/background.png")))));
-     	} catch (IOException e) {
-     		e.printStackTrace();
-     	}
+	    setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/res/background.png")))));
+	    canvas.setFont(Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/res/SourceSansPro-Light.ttf")));
+	} catch (FontFormatException | IOException e) {
+	    e.printStackTrace();
+	}
 	//Background optimieren:
 	//https://tips4java.wordpress.com/2008/10/12/background-panel/
 	//TODO: Herr Hanauska fragen, ob das geht.
-	
-	//setLayout(new BoxLayout(getContentPane(), 0));
-	setLayout(new BorderLayout());
 
-	canvas.setURI(new File("src/res/gameboard.svg").toURI().toString());
-	try{
-	canvas.setFont(Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/res/SourceSansPro-Light.ttf")));
-	} catch (FontFormatException | IOException e){
-	    e.printStackTrace();
-	}
-	
 	canvas.setAutoscrolls(true);
-	
+
 	this.addComponentListener(new ComponentAdapter() {
 	    @Override
 	    public void componentResized(ComponentEvent e) {
@@ -74,22 +65,27 @@ public class view extends javax.swing.JFrame {
 		SVGDocument svgd = canvas.getSVGDocument();
 	    }
 	});
-	
+
 	//http://stackoverflow.com/questions/22010658/boxlayout-inside-a-borderlayout
-	
 	JPanel buttons = new JPanel();
 	JPanel chat = new JPanel();
 	buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
 	buttons.setLayout(new BoxLayout(buttons, BoxLayout.Y_AXIS));
-	
-	JButton btn1 = new JButton("Hello World 1");
+
+	JButton btn1 = new JButton("Move to LOS");
+
+	btn1.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+	    }
+	});
+
 	JButton btn2 = new JButton("Hello World 2");
 	JButton btn3 = new JButton("Hello World 3");
 	JButton btn4 = new JButton("Hello World 4");
 	JButton btn5 = new JButton("Hello World 5");
 	JButton btn6 = new JButton("Hello World 6");
 	JButton btn7 = new JButton("Me is chat box");
-	
+
 	buttons.add(btn1);
 	buttons.add(btn2);
 	buttons.add(btn3);
@@ -97,7 +93,7 @@ public class view extends javax.swing.JFrame {
 	buttons.add(btn5);
 	buttons.add(btn6);
 	chat.add(btn7);
-	
+
 	add(buttons, BorderLayout.WEST);
 	add(canvas, BorderLayout.CENTER);
 	add(chat, BorderLayout.EAST);
