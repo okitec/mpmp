10e4b4e97d945f5ef16a278530bef1cf78632238
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-06-25 21:27:05 +0200

    updated Displayer javadoc comment

--- mpmp/src/view/Displayer.java
+++ mpmp/src/view/Displayer.java
@@ -1,7 +1,11 @@
 package view;
 
 /**
- * Displayer provides an interface for a text box (e.g. the chat box or the player list).
+ * Displayer provides an interface for some kind of display, e.g. the chat box or the player list.
+ * The caller can't assume much; show somehow displays a message; the reset method clears the current
+ * contents if that makes sense (is is often a nil function).
+ *
+ * @author oki
  */
 public interface Displayer {
 	public void show(String s);
