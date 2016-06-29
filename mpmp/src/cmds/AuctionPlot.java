064e290cc25991d7fd1c66df89a495cb65ef7da6
Leander Dreier <leander3@outlook.de>
2016-06-29 14:23:15 +0200

    Removed licence header

--- mpmp/src/cmds/AuctionPlot.java
+++ mpmp/src/cmds/AuctionPlot.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cmds;
 
 import java.util.Arrays;
@@ -13,7 +8,7 @@ import model.Plot;
 import view.Displayer;
 
 /**
- *
+ * S->C
  * @author Leander
  */
 public class AuctionPlot implements CmdFunc {
--- mpmp/src/cmds/Hypothec.java
+++ mpmp/src/cmds/Hypothec.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cmds;
 
 import java.util.Arrays;
@@ -14,7 +9,7 @@ import model.Plot;
 import view.Displayer;
 
 /**
- *
+ * C->S
  * @author Leander
  */
 public class Hypothec implements CmdFunc {
