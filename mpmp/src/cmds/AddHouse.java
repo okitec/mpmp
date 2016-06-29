972ae8b8efe3e1d7f45a83a9d57ed1720b810719
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-06-29 20:57:16 +0200

    Revert "Copy pase adding"

--- mpmp/src/cmds/AddHouse.java
+++ mpmp/src/cmds/AddHouse.java
@@ -55,7 +55,6 @@ public class AddHouse implements CmdFunc {
 		case 1:
 			conn.sendOK();
 			conn.send("show-transaction " + plot.getHousePrice(plot.getHouses()) + " Buy house for plot " + plot.getName());
-			conn.send("money-update " + plot.getHousePrice(plot.getHouses()) + " Buy house for plot " + plot.getName());
 			conn.send("plot-update " + plot.getName() + " " + plot.getHouses() + " " + plot.isHypothec() + plot.getOwner());
 			break;
 		default:
