dc5dc69bc8cf2b4742505afdc246e14fd302b17a
Matthias Fersch <matthias.fersch97@gmail.com>
2016-06-29 23:19:22 +0100

    Add community and event fields

--- mpmp/src/model/Field.java
+++ mpmp/src/model/Field.java
@@ -3,14 +3,19 @@ package model;
 /**
  * List of special fields and number of fields in total.
  *
- * @author oki
+ * @author oki, Matthias
  */
 public class Field {
-	public static final int IncomeTax   =  4;
-	public static final int Prison      = 10;
-	public static final int FreeParking = 20;
-	public static final int Police      = 30;
-	public static final int ExtraTax    = 38;
-
-	public static final int Nfields     = 40;
+	public static final int CommunityField1 =  2;
+	public static final int IncomeTax       =  4;
+	public static final int EventField1     =  7;
+	public static final int Prison          = 10;
+	public static final int CommunityField2 = 17;
+	public static final int FreeParking     = 20;
+	public static final int EventField2     = 23;
+	public static final int Police          = 30;
+	public static final int CommunityField3 = 33;
+	public static final int EventField3     = 36;
+	public static final int ExtraTax        = 38;
+	public static final int Nfields         = 40;
 }
