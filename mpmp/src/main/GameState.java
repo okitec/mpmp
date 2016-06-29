65e60d602bc0aeae0f2147bc15926174530078dd
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-06-30 00:49:08 +0200

    Fix instant disconnect

--- mpmp/src/main/GameState.java
+++ mpmp/src/main/GameState.java
@@ -7,15 +7,15 @@ package main;
 public enum GameState {
 	Pregame, RunningGame;
 
-	private static GameState state;
+	private static GameState state = Pregame;
 
 	public static void startGame() {
 		state = RunningGame;
 	}
 
 	public static boolean running() {
-		if (state == Pregame)
-			return false;
-		return true;
+		if (state == RunningGame)
+			return true;
+		return false;
 	}
 }
