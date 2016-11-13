d87903d86c4127b92d8002174f1bb7c65f6439c9
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-11-13 20:14:41 +0100

    Add marking fields; unfinished

--- mpmp/src/view/Gameboard.java
+++ mpmp/src/view/Gameboard.java
@@ -21,6 +21,15 @@ import org.apache.batik.swing.gvt.GVTTreeRendererEvent;
  * The gameboard holds the gameboard SVG and draws the players upon it.
  */
 public class Gameboard extends JSVGCanvas {
+	// 0.258: scale transform set in the gameboard SVG internally
+	private static final double SVGScale = 0.258;
+
+	// Height and width of original unresized portrait field.
+	private static final int WFLD = 304;
+	private static final int HFLD = 506;
+
+	private static final int MarkedPlotAlpha = 100; // 0-255; here: less than 50%
+
 	private Frame f;
 	private Model m;
 	private Converter converter;
@@ -28,7 +37,7 @@ public class Gameboard extends JSVGCanvas {
 	public Gameboard(Frame f, Model m) {
 		this.f = f;
 		this.m = m;
-		converter = new Converter(304, 506);	  // XXX magic: original unresized wfld, hfld
+		converter = new Converter(WFLD, HFLD);
 	}
 
 	public void init() {
@@ -66,6 +75,12 @@ public class Gameboard extends JSVGCanvas {
 		for (Player p : m.getPlayers()) {
 			drawPlayer(p);
 		}
+
+		// debug draws
+		markPlot(9, Color.RED);
+		markPlot(11, Color.GREEN);
+		markPlot(24, Color.BLUE);
+		markPlot(39, Color.YELLOW);
 	}
 
 	/**
@@ -81,9 +96,8 @@ public class Gameboard extends JSVGCanvas {
 		Graphics g = getGraphics();
 
 		Point pt = converter.middleRelPx(p.getPos());
-		// 0.258: scale transform set in the gameboard SVG internally
-		pt.x *= 0.258 * scale;
-		pt.y *= 0.258 * scale;
+		pt.x *= SVGScale * scale;
+		pt.y *= SVGScale * scale;
 		pt.x += at.getTranslateX();
 		pt.y += at.getTranslateY();
 		rOuter *= scale;
@@ -93,6 +107,41 @@ public class Gameboard extends JSVGCanvas {
 		g.fillOval(pt.x - rOuter, pt.y - rOuter, 2 * rOuter, 2 * rOuter);
 		g.setColor(p.getColor());
 		g.fillOval(pt.x - rInner, pt.y - rInner, 2 * rInner, 2 * rInner);
+	}
+
+	/**
+	 * Mark a plot with a transparent shade.
+	 */
+	private void markPlot(int pos, Color col) {
+		double scale;
+		int dx, dy;
+
+		AffineTransform at = getViewBoxTransform();
+		Graphics g = getGraphics();
+		scale = getScale();
+
+		Point pt = converter.cornerRelPx(pos); // left upper corner
+		if(converter.isPortrait(pos)) {
+			dx = WFLD;
+			dy = HFLD;
+		} else if(converter.isLandscape(pos)) {
+			// Reverse height and width
+			dx = HFLD;
+			dy = WFLD;
+		} else {
+			return;  // corner fields are never plots
+		}
+
+		pt.x *= SVGScale * scale;
+		pt.y *= SVGScale * scale;
+		pt.x += at.getTranslateX();
+		pt.y += at.getTranslateY();
+		dx *= SVGScale * scale;
+		dy *= SVGScale * scale;
+
+		Color shade = new Color(col.getRed(), col.getGreen(), col.getBlue(), MarkedPlotAlpha);
+		g.setColor(shade);
+		g.fillRect(pt.x, pt.y, dx, dy);
 	}
 
 	public double getScale() {
